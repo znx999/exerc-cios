09790561719bbb63d9f9bf2e7915668f2e2759a6
znx999 <weslley.alves3310@gmail.com>
2024-04-22 10:20:05 -0300

    exercicio das classificação dos triângulos quanto aos lados

---
+++ exercise/src/exercise/triangulo.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class triangulo {
+	public static void main(String [] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o vlor 1: ");
+		int t1 = scanner.nextInt();
+		
+		
+		System.out.print("Digite o vlor 3: ");
+		int t2 = scanner.nextInt();
+		
+		
+		System.out.print("Digite o vlor 2: ");
+		int t3 = scanner.nextInt();
+		
+		scanner.close();
+		
+		if (t1 > 0 && t2 > 0 && t3 > 0 && (t1 + t2 > t3 && t1 + t3 > t2 && t2 + t3 > t1) ) {
+			if (t1 != t2 && t1 != t3 && t2 != t3) {
+			System.out.print("É um triangulo escaleno! ");
+			} else if (t1 == t2 && t1 == t3 && t2 == t3) {
+				System.out.print("É um triangulo equilatero! ");
+			} else {
+				System.out.print("É um triangulo isosceles");
+			}
+		} else {
+			System.out.print("Ao menos um dos valores indicados não servem para formar um triangulo!");
+		}
+	}
+
+}
